c726a39adddfb432800402b018153c393fdc97ba
Keilla <keilla@gmail.com>
2019-06-06 17:56:03 -0300

    Updated exercises

--- Chapter 1/exercise-2/src/app/app.module.ts
+++ Chapter 1/exercise-2/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
@@ -12,7 +11,6 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Chapter 1/exercise-4/src/app/app.module.ts
+++ Chapter 1/exercise-4/src/app/app.module.ts
@@ -3,15 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
+    LoginModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
