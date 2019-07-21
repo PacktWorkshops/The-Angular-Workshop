90ede8528bdc35e8affc1ca9af42c30a8a65e691
Keilla Menezes Fernandes <keilla@gmail.com>
2019-07-21 18:16:42 -0300

    Exercise 5: Create a custom pipe

---
+++ Chapter 2/exercise-5/src/app/trip-plan/trip-plan.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { TripStep } from '../enums/trip-step';
+
+@Component({
+  selector: 'app-trip-plan',
+  templateUrl: './trip-plan.component.html',
+  styleUrls: ['./trip-plan.component.css']
+})
+export class TripPlanComponent implements OnInit {
+
+  hour: number;
+  stations: string[];
+  tripStep = TripStep;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.hour = Date.now();
+
+    this.stations = ['Apollo Ave.', 'Oaks Bottom', 'Developer Dr'];
+  }
+
+}
