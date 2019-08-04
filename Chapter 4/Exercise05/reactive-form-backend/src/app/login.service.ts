12df0bc5bffa7177b2144b44ef9e3094850d2525
Kevin.Hennessy <marykevinh@gmail.com>
2019-08-03 20:56:15 -0400

    Chapte4 Exercise 5 start

---
+++ Chapter 4/Exercise05/reactive-form-backend/src/app/login.service.ts
@@ -0,0 +1,45 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { User } from './user';
+import { LoginResponse } from './login-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      ContentType: 'application/json'
+      }
+    )
+  };
+
+  constructor(private httpClient: HttpClient) { }
+
+  login(user: User): Observable<LoginResponse | boolean> {
+    const body = {
+      username: user.name,
+      password: user.password
+    };
+
+    return this.httpClient.post<LoginResponse>('api/auth/login', body, this.httpOptions)
+      .pipe(
+        catchError(this.handleError())
+      );
+  }
+
+  private handleError(operation = 'operation', result?) {
+    return (error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        return of(false);
+      } else {
+        console.error(error);
+        return throwError('An error occurred:', error.error.message);
+      }
+    };
+  }
+}
+
--- Chapter 4/Exercise05/reactive-form-backend/src/app/login/login.component.ts
+++ Chapter 4/Exercise05/reactive-form-backend/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from '../user';
 import { LoginService } from '../login.service';
 
@@ -24,17 +24,19 @@ export class LoginComponent implements OnInit {
 
   buildLoginForm() {
     this.loginForm = this.formBuilder.group({
-      name: [this.user.name],
-      password: [this.user.password]
+      name: [this.user.name, [Validators.required, Validators.email]],
+      password: [this.user.password, Validators.required]
     });
   }
 
   onSubmit() {
-    this.loginService.login(this.loginForm.value).subscribe(
-      (data) => {
-        this.loggedIn = !!data;
-        this.submitted = true;
-      }
-    );
+    if (this.loginForm.valid) {
+      this.loginService.login(this.loginForm.value).subscribe(
+        (data) => {
+          this.loggedIn = !!data;
+          this.submitted = true;
+        }
+      );
+    }
   }
 }
